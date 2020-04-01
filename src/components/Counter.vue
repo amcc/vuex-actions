f4bd96844631ce3eab976ff4c6bf397cb5f5adba
amcc <alistairmcclymont@gmail.com>
2020-04-01 19:02:43 +0100

    first commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Counter!!!</h1>
+    <p>{{count}}</p>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex';
+export default {
+  name: 'Counter',
+  computed: mapState(['count']),
+  methods: {
+    ...mapActions(['increment', 'decrement'])
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,30 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    count: 10
+  },
+  mutations: {
+    increment(state) {
+      state.count++;
+    },
+    decrement(state) {
+      state.count--;
+    }
+  },
+  actions: {
+    increment({ commit }) {
+      setTimeout(() => {
+        commit("increment");
+      }, 1000);
+      // commit("increment");
+    },
+    decrement({ commit }) {
+      commit("decrement");
+    }
+  },
+  modules: {}
+});
